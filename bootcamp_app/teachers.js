eaf355d4713392f7eaec81a7794356ac1bc34a5d
JP <kalambayjp@gmail.com>
2022-01-06 16:50:00 -0800

    parameterized nodePostgres queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -6,15 +6,17 @@ const pool = new Pool({
   host: 'localhost',
   database: 'bootcampx'
 });
+const cohortName = `%${process.argv[2]}%`;
+const values = [cohortName];
 
 pool.query(`SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
 FROM cohorts
 JOIN students ON students.cohort_id = cohorts.id
 JOIN assistance_requests ON assistance_requests.student_id = students.id 
 JOIN teachers ON teachers.id = assistance_requests.teacher_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
+WHERE cohorts.name LIKE $1
 ORDER BY teacher;
-`)
+`, values)
 .then(res => {
   res.rows.forEach(teacher => {
     console.log(`${teacher.cohort}: ${teacher.teacher}`);
